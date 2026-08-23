a85600e930158e4ba012983e694b61f6f20869db
Muntajir Alam <muntajiralam20074@gmail.com>
2026-08-23 19:57:45 +0530

    chore: update package.json formatting and remove unnecessary whitespace
    feat: implement database connection in db.js
    
    feat: add initial structure for authentication and order controllers
    
    feat: create middleware for authentication, role management, and validation
    
    feat: add models for user, order, and product
    
    feat: establish routes for authentication, orders, and products
    
    feat: implement validators for authentication, orders, and products

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import app from './src/app.js';
 import connectDB from './src/config/db.js';
 
-
 connectDB();
 
 const port = 3000;
--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,6 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-
 const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB connected');
